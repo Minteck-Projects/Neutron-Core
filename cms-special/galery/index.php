b180a0515a72e534790a263aea89cb4da1d8d6e5
Minteck <freeziv.ytb@gmail.com>
2019-10-02 22:38:16 +0200

    ctrl 2 10 19, 22:38

--- cms-special/galery/index.php
+++ cms-special/galery/index.php
@@ -9,25 +9,37 @@ function buffer(string $value) {
 }
 
 if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/enabled")) {
-    buffer("<center><div id=\"galery_thumbnails\"><center>");
-    $photos = scandir($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/pictures");
-    foreach ($photos as $photo) {
-        if ($photo == "." || $photo == "..") {} else {
-            $praw = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/pictures/" . $photo);
-            $pcat = explode("|", $praw)[1];
-            $ppath = explode("|", $praw)[0];
-            buffer("<div class=\"photo\">");
-            buffer("<a href=\"/cms-special/galery/preview/?url=" . $ppath . "\"><img class=\"photo_image\" src=\"" . $ppath . "\" /></a><b>");
-            if ($pcat == "unclassed") {
-                buffer("Non classé");
+    $categories = scandir($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/categories");
+    array_push($categories, "unclassed");
+    foreach ($categories as $category) {
+        if ($category != "." && $category != "..") {
+            $shown = false;
+            if ($category == "unclassed") {
+                buffer("<h2>Non classé</h2>");
             } else {
-                buffer(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/categories/" . $pcat));
+                buffer("<h2>" . file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/categories/" . $category) . "</h2>");
             }
-            buffer("</b>");
-            buffer("</div>");
+            buffer("<center><div id=\"galery_thumbnails\"><center>");
+            $photos = scandir($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/pictures");
+            foreach ($photos as $photo) {
+                if ($photo == "." || $photo == "..") {} else {
+                    $praw = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/data/webcontent/galery/pictures/" . $photo);
+                    $pcat = explode("|", $praw)[1];
+                    $ppath = explode("|", $praw)[0];
+                    if ($pcat == $category) {
+                        $shown = true;
+                        buffer("<div class=\"photo\">");
+                        buffer("<a href=\"/cms-special/galery/preview/?url=" . $ppath . "\"><img class=\"photo_image\" src=\"" . $ppath . "\" /></a>");
+                        buffer("</div>");
+                    }
+                }
+            }
+            if (!$shown) {
+                buffer("<p><center><i>Aucun photo dans cette catégorie</i></center></p>");
+            }
+            buffer("</center></div></center>");
         }
     }
-    buffer("</center></div></center>");
 } else {
     buffer("<center><i>La <b>galerie de photos</b> n'est pas activée sur ce site</i></center>");
 }
